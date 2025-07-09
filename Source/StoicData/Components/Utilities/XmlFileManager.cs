2d328e9bb67c9cef22575234580a10cf4caf239e
zVoidra <69354246+zVoidra@users.noreply.github.com>
2025-07-09 20:09:55 +0200

    SaveToXml File.

---
+++ Source/StoicData/Components/Utilities/XmlFileManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Linq;
+
+namespace StoicData.Components.Utilities;
+
+internal static class XmlFileManager
+{
+  private const string DefaultFilePath = @"C:\temp\StoicQuote";
+
+  public static void SaveToXml(string quote, string author)
+  {
+    XDocument doc = new XDocument(
+      new XElement("StoicQuote",
+        new XElement("Quote", quote),
+        new XElement("Author", author)
+      )
+    );
+
+    if (Directory.Exists(Path.GetDirectoryName(DefaultFilePath)) == false)
+    {
+      Directory.CreateDirectory(Path.GetDirectoryName(DefaultFilePath));
+    }
+
+    doc.Save(DefaultFilePath + DateTime.Now.ToString("_yyyy-mm-dd_HH-mm-ss") + ".xml");
+  }
+}
